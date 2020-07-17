fae085d6b2257603e995b17e3c91a7775d57473d
Bec Sun <rebeccasun86@gmail.com>
2020-07-17 21:32:41 +0100

    got the poo to move side to side without going out of the screen. Want to look into not hard coding the condional

--- scripts/app.js
+++ scripts/app.js
@@ -6,7 +6,7 @@ function init() {
   const width = 10
   const numberOfCells = width * width
   //position of the colour tube
-  const tube = 94
+  let tube = 94
   let client = null
   // make the grid
   function startGame() {
@@ -18,6 +18,10 @@ function init() {
       grid.appendChild(cell)
     }
 
+    cells[tube].classList.add('poo')
+
+   
+
     //making all the clients
     let client = [2, 3, 4, 5, 6, 7, 12, 13, 14, 15, 16, 17, 22, 23, 24, 25, 26, 27, 32, 33, 34, 35, 36, 37, 42, 43, 44, 45, 46, 47]
     console.log(client)
@@ -26,13 +30,52 @@ function init() {
       cells[person].classList.add('fire')
     })
     //testing the layout
-    cells[tube].classList.add('poo')
-  }
-
     
+    
+     //move clients
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+  window.addEventListener('keydown', handleKeyDown)
+   
+
+  function handleKeyDown(e) {
+   console.log(tube)
+   cells[tube].classList.remove('poo')
+    switch (e.keyCode) {
+      //RIGHT
+      case 39:
+        if (tube < 99) {
+          tube++
+        }
+        break
+      //LEFT
+      case 37:
+        if (tube > 90 ) {
+          tube--
+        }
+        break
+      default:
+        break
+      }
+      cells[tube].classList.add('poo')
  
-
-
+    }
 startGame()
+
+
+
 }
 window.addEventListener('DOMContentLoaded', init)
