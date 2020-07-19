9a7a17de9e7baf6136201998801269cda6e003aa
Bec Sun <rebeccasun86@gmail.com>
2020-07-19 07:56:58 +0100

    moved all the client div's to the left so it would be easier to start the game. Code commented out the remove all of them in order to put the next line of code to move to the right and at back on the page

--- scripts/app.js
+++ scripts/app.js
@@ -1,46 +1,67 @@
 function init() {
 
   const grid = document.querySelector('.grid')
+  const moneyMade = document.querySelector('#money-display')
+  const highestScore = document.querySelector('#highest-display')
+
   const cells = []
 
-  const width = 10
+
+  let width = 10
   const numberOfCells = width * width
   //position of the colour tube
   let tube = 94
-  let client = null
+  let clients = null
   // make the grid
   function startGame() {
     for (let i = 0; i < numberOfCells; i++) {
       const cell = document.createElement('div')
       cells.push(cell)
-      //maybe delete the number later
+      //maybe delete the i later
       cell.innerHTML = i
       grid.appendChild(cell)
     }
 
     cells[tube].classList.add('poo')
 
-   
+
 
     //making all the clients
-    let client = [2, 3, 4, 5, 6, 7, 12, 13, 14, 15, 16, 17, 22, 23, 24, 25, 26, 27, 32, 33, 34, 35, 36, 37, 42, 43, 44, 45, 46, 47]
-    console.log(client)
+    let clients = [0, 1, 2, 3, 4, 5, 6, 10, 11, 12, 13, 14, 15, 16, 20, 21, 22, 23, 24, 25, 26, 30, 31, 32, 33, 34, 35, 36, 40, 41, 42, 43, 44, 45, 46]
+    console.log(clients)
 
-    client.forEach(function (person) {
-      cells[person].classList.add('fire')
+    let groupedClients = clients.forEach(client => {
+      cells[client].classList.add('fire')
     })
     //testing the layout
-    
-    
-     //move clients
+
+    // move clients
+    clientsAttack()
+
+    function clientsAttack() {
+      clients.forEach(client => {
+        cells[client].classList.remove('fire')
+        console.log(cells[client])
+      })
+      console.log(clients.length)
+      const x = cells[clients] % width
+      console.log(x)
+      const y = Math.floor(cells[clients] / width)
+    }
 
 
+    clients.forEach(client => {
+      cells[client].classList.add('fire')
 
+    })
+    // for (let client in clients){
+    //   cells[client].classList.remove('fire')
+    // }
+    // for (let i = 0; i < 10; clients.length){
+    //   clients[i] = clients[i] + 1 
+    //  }
 
-
-
-
-
+    // setInterval( clientAttacks(), 3000)
 
 
 
@@ -49,11 +70,11 @@ function init() {
 
   }
   window.addEventListener('keydown', handleKeyDown)
-   
+
 
   function handleKeyDown(e) {
-   console.log(tube)
-   cells[tube].classList.remove('poo')
+    console.log(tube)
+    cells[tube].classList.remove('poo')
     switch (e.keyCode) {
       //RIGHT
       case 39:
@@ -61,19 +82,19 @@ function init() {
           tube++
         }
         break
-      //LEFT
+        //LEFT
       case 37:
-        if (tube > 90 ) {
+        if (tube > 90) {
           tube--
         }
         break
       default:
         break
-      }
-      cells[tube].classList.add('poo')
- 
     }
-startGame()
+    cells[tube].classList.add('poo')
+
+  }
+  startGame()
 
 
 
